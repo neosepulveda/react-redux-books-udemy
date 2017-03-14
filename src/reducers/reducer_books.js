022b403f80f49f8f0e0dc3107fc0cb1c7df4e691
Neo Sepulveda <israel.sepulveda.castillejo@gmail.com>
2017-03-14 21:46:59 +0000

    Rendering a list of books. React connected to Redux

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Javascript: The Good Parts' },
+    { title: 'Lord Of The Rings' },
+    { title: 'The Dark Tower' },
+    { title: 'Eloquent Ruby' }
+  ]
+}
