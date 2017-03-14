c6b4c17052a35aa7d0d6305a956939366b610d09
Neo Sepulveda <israel.sepulveda.castillejo@gmail.com>
2017-03-14 23:08:36 +0000

    Added props.book.pages to spice things up

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,8 +1,8 @@
 export default function() {
   return [
-    { title: 'Javascript: The Good Parts' },
-    { title: 'Lord Of The Rings' },
-    { title: 'The Dark Tower' },
-    { title: 'Eloquent Ruby' }
+    { title: 'Javascript: The Good Parts', pages: 101 },
+    { title: 'Lord Of The Rings', pages: 504 },
+    { title: 'The Dark Tower', pages: 221 },
+    { title: 'Eloquent Ruby', pages: 301 }
   ]
 }
