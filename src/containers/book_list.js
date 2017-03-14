6f8264b856d5369378dd59cf73e3a15c27105348
Neo Sepulveda <israel.sepulveda.castillejo@gmail.com>
2017-03-14 22:27:09 +0000

    Added selectBook action

--- src/containers/book_list.js
+++ src/containers/book_list.js
@@ -1,11 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+
+import { selectBook } from '../actions/index';
 
 class BookList extends Component {
   renderList() {
     return this.props.books.map((book) => {
       return (
-        <li key={book.title} className="list-group-item">{book.title}</li>
+        <li
+          key={book.title}
+          onClick={() => this.props.selectBook(book)}
+          className="list-group-item">
+          {book.title}
+        </li>
       );
     });
   }
@@ -27,4 +35,14 @@ function mapStateToProps(state) {
   };
 }
 
-export default connect(mapStateToProps)(BookList);
+//Anything returned from this function will end up as props
+//on the BookList container
+function mapDispatchToProps(dispatch) {
+  //Whenever select book is called, the result should be passed
+  //to all of our reducers.
+  return bindActionCreators({ selectBook: selectBook }, dispatch);
+}
+
+//Promote BookList from a Component to a container - it needs to know
+//about this new dispatch method, selectBook. Make it available as a prop.
+export default connect(mapStateToProps, mapDispatchToProps)(BookList);
